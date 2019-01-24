0a1a9695ae4d271d08e176bc3b81e3880745dd4e
Lauren E. Hostert <40471257+lehostert@users.noreply.github.com>
2019-01-24 17:10:18 -0600

    Update Vegan_Demo_20190125.R
    Added a fucntion for evenness

--- Multivariate_Analysis/Vegan_Demo_20190125.R
+++ Multivariate_Analysis/Vegan_Demo_20190125.R
@@ -1,6 +1,43 @@
 library(tidyverse)
 library(vegan)
 
-fsh <- read.csv("~/Github/Stats_Workshop_SEL/Multivariate_Analysis/five_assemblages.csv", header = T, na = ".", row.names = 1)
+data(BCI)
 
+fsh <- read.csv("~/Github/Stats_Workshop_SEL/Multivariate_Analysis/five_assemblages.csv", header = T, na = ".", row.names = 1)
+LTEF <- read.csv("~/Github/Stats_Workshop_SEL/Multivariate_Analysis/LTEF_fish_data.csv", header = T, na = ".", row.names = 1)
+
+evenness  <- function(x) {
+  diversity(x)/log(specnumber(x))
+}
+
+H <- diversity(fsh, index = "shannon")
+J <- evenness(fsh)
+BC <- vegdist(fsh, method = "bray")
 CY <- vegdist(fsh, method = "cao")
+
+Fish_Summary <- as.data.frame(H)
+Fish_Summary$Evenness <- J
+
+
+Dissimilarity <- as.matrix(BC)
+
+# CY <- vegdist(fsh, method = "cao")
+# y <- diversity(BCI, index = "shannon")
+# yy <- diversity(BCI)
+# z <- diversity (fsh, index = "shannon")
+# 
+# Z <- as.data.frame(z)
+# 
+# Fish_Summary <- as.data.frame(y)
+# Fish_Summary$yy <- y
+# 
+# J_hat <- evenness(BCI)
+# S <- specnumber(BCI)
+# 
+# yak$J <-(diversity(BCI)/log(S))
+# 
+# evenness  <- function(x) {
+#   diversity(x)/log(specnumber(x))
+# }
+# 
+# J_hat <- evenness(BCI)
